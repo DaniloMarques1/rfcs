0db6a56095d79de818d43fc96090f1e4624c65c7
Danilo Marques <danilomarques20@hotmail.com>
2021-01-08 22:14:53 -0300

    Adding a simple jwt(7519) implementation

---
+++ 7519/jwt.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Header struct {
+	Typ string `json:"typ"`
+	Alg string `json:"alg"`
+}
+
+type Jwt struct {
+	Header    Header
+	Payload   map[string]interface{}
+	Signature string
+}
+
+//  Will create token following jwt's requirements
+func Sign(payload map[string]interface{}, secret string) string {
+	// TODO add exp time claim
+	var jwt Jwt
+	// default header values
+	jwt.Header.Typ = "JWT"
+	jwt.Header.Alg = "HS256" // at least for now, only sha256
+	jwt.Payload = payload
+	marshedHeader, err := json.Marshal(&jwt.Header)
+	if err != nil {
+		fmt.Printf("Error %v \n", err)
+		os.Exit(-1)
+	}
+	encodedHeader := base64.RawURLEncoding.EncodeToString([]byte(marshedHeader)) // first part
+
+	jwt.Payload["iat"] = time.Now().Unix()
+	marshedPayload, err := json.Marshal(&jwt.Payload)
+	if err != nil {
+		fmt.Printf("Error %v \n", err)
+		os.Exit(-1)
+	}
+	encodedPayload := base64.RawURLEncoding.EncodeToString([]byte(marshedPayload)) // second part
+
+	jwt.Signature = signPayloadAndHeader(encodedHeader, encodedPayload, secret) // third part
+	token := fmt.Sprintf("%s.%s.%s", encodedHeader, encodedPayload, jwt.Signature)
+	return token
+}
+
+// checks if token is valid, if so returns a map with the payload
+func Verify(token, privateKey string) (map[string]interface{}, error) {
+	splitedToken := strings.Split(token, ".")
+	payload, err := base64.RawURLEncoding.DecodeString(splitedToken[1])
+	if err != nil {
+		fmt.Printf("Error decoding payload %v", err)
+		return nil, err
+	}
+	fmt.Println(string(payload))
+	payloadMap := make(map[string]interface{})
+	err = json.Unmarshal(payload, &payloadMap)
+	if err != nil {
+		fmt.Printf("Error decoding payload %v", err)
+		return nil, err
+	}
+
+	return payloadMap, nil
+}
+
+// will sign the token header and paylaod using the given privatekey
+// and the hmac sha256 algorithm, returning the base64url encoded signed string
+func signPayloadAndHeader(encodedHeader, encodedPayload, privateKey string) string {
+	//privateKey = base64.RawURLEncoding.EncodeToString([]byte(privateKey))
+	h := hmac.New(sha256.New, []byte(privateKey))
+	concat := fmt.Sprintf("%s.%s", encodedHeader, encodedPayload)
+	h.Write([]byte(concat))
+	return base64.RawURLEncoding.EncodeToString(h.Sum(nil))
+}
+
+func main() {
+	payload := make(map[string]interface{})
+	payload["id"] = 123
+	secret := "secret"
+	token := Sign(payload, secret)
+	fmt.Println(token)
+	Verify(token, secret)
+}
